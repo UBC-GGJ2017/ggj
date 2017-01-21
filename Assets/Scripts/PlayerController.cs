809105d3dbca14897a0302f4bde1ae8f68982b89
anilburakbilsel <abbilsel@gmail.com>
2017-01-21 14:43:59 -0800

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,36 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+
+    private float speed = 1;
+
+    private int spacing = 1;
+
+    private Vector2 pos;
 
 	private Rigidbody2D rb;
-    private Animator anim;
 
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pos = transform.position;
+    }
 
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-		Vector2 movement = new Vector2 (moveHorizontal, 0.0f);
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
 
-		rb.AddForce (movement * speed);
+        if (Input.GetKeyDown(KeyCode.W))
+            pos.y += spacing;
+        if (Input.GetKeyDown(KeyCode.S))
+            pos.y -= spacing;
+        if (Input.GetKeyDown(KeyCode.A))
+            pos.x -= spacing;
+        if (Input.GetKeyDown(KeyCode.D))
+            pos.x += spacing;
 
-        // Animation
-        anim.SetFloat("SpeedX", Mathf.Abs(rb.velocity.x));
-        anim.SetFloat("SpeedY", Mathf.Abs(rb.velocity.y));
-	}
+        transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+    }
 }
