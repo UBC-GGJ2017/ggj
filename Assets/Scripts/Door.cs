f73b06d4f470a39e6ab30126b175dd40e5662a3b
Michael Hou <Michael@dhcp-206-87-129-0.ubcsecure.wireless.ubc.ca>
2017-01-21 10:38:06 -0800

    added door and door physics

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player")
+			print ("door crash");
+
+	}
+}
